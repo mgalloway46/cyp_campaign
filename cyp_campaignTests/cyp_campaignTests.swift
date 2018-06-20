eda6d0739a764d780659949f68e2b996ecac8d62
Matt Galloway <mattgalloway22@me.com>
2018-06-19 20:06:49 -0500

    Initial Commit

---
+++ cyp_campaignTests/cyp_campaignTests.swift
@@ -0,0 +1,36 @@
+//
+//  cyp_campaignTests.swift
+//  cyp_campaignTests
+//
+//  Created by Matt Galloway on 6/19/18.
+//  Copyright © 2018 Matt Galloway. All rights reserved.
+//
+
+import XCTest
+@testable import cyp_campaign
+
+class cyp_campaignTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
